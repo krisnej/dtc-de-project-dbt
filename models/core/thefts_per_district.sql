5e72804bf5a93f9fc1e511a587f58ef321ba0170
kristina@nejdl.eu <kristina@nejdl.eu>
2022-04-16 11:04:53 +0000

    add theft_by_district view

---
+++ models/core/thefts_per_district.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='view') }}
+
+SELECT
+    time_of_crime_start_day as date_start,
+    time_of_crime_start_hour as hour,
+    lor_name,
+    district_name,
+    amount_of_damage
+FROM {{ ref('stg_bike_data_cleaned') }} b
+join {{ ref('stg_lor_data_cleaned') }} l
+    on b.LOR=l.lor_key
+join {{ ref('stg_districts_data_cleaned') }} d
+    on l.district_key=d.district_key
+WHERE 
+    attempt is False
